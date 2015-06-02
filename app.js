7e2f73b481361241edc8e1f20cf884ed9e8a5076
Aeshan Wijetunge <awijetunge@paypal.com>
2015-06-02 15:17:35 +0000

    weather stuff

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 
 // This application uses express as it's web server
 // for more info, see: http://expressjs.com
-var express = require('express');
+var express = require('express'),
+    adaro = require('adaro'),
+    request = require('request');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,15 +17,60 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var options = {
+  helpers: [
+    //NOTE: function has to take dust as an argument.
+    //The following function defines @myHelper helper
+    function (dust) { dust.helpers.myHelper = function (a, b, c, d) {} },
+    'dustjs-helpers'   //So do installed modules
+  ]
+};
+
+app.engine('dust', adaro.dust(options));
+app.set('view engine', 'dust');
+
+app.set('views', __dirname + '/public/views');
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/home',function(req, res) {
+    req.model = {};
+    req.model.name = 'SST';
+    
+    res.render('index',req.model);
+});
+
+app.get('/weather', function(req, res){
+   request('http://api.openweathermap.org/data/2.5/weather?q=Singapore', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('response.body:' + body); // Weather Data for Singapore
+        
+        //Note that we should parse the stringified response to JSON
+        var result = JSON.parse(body);
+        
+        console.log('result: ' + result);
+        
+        //Populate the view model used to display
+        req.model = {};
+        req.model.country = result.name,
+        req.model.coord = result.coord,
+        req.model.weather = result.weather
+        
+        
+        console.log('req.model: %j',req.model);
+        
+        res.render('weather',req.model);
+      }
+    });
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, appEnv.bind, function() {
-
+app.listen(appEnv.port, function() {
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
